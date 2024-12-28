2aeecc948764d208544b9f78a13d9b19fc02dc68
Akshay peherkar <95123298+Akshayp02@users.noreply.github.com>
2024-12-28 16:47:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+   <>
+   <h1 className='text-center text-rose-600 text-18  text-2xl rounded-md' >Welcome to the Sanchi Tour's And Travals</h1>
+
+   </>
+  );
+}
+
+export default App;
